51b27762a8d263be6a33b72673e09a94f9241aa5
Dan Oh <danoh82@gmail.com>
2018-05-06 22:23:51 -0700

    results are injected to html; WIP: add user inputs into params

--- logic.js
+++ logic.js
@@ -5,7 +5,7 @@ let startDate = 0;
 let endDate = 0;
 let sortType;
 let queryURLBase = `https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${apiKey}&q=trump`;
-let articleCounter = 0;
+let articleNumber = 0;
 //byline.original
 runQuery = (numOfArticles, queryURL) => {
     $
@@ -17,19 +17,42 @@ runQuery = (numOfArticles, queryURL) => {
             console.log(queryURL);
             console.log(result);
             console.log(result.response.docs[0].headline.main)
+            console.log(result.response.docs[0].web_url);
             for (var i = 0; i < result.response.docs.length; i++) {
+                articleNumber++
                 let author = result.response.docs[i].byline.original;
                 let title = result.response.docs[i].headline.main;
                 let image = result.response.docs[i].multimedia[0].url;
+                let link = result.response.docs[i].web_url;
+                let snippet = result.response.docs[i].snippet;
+                //create a card for each 
+                let card = $("<div>");
+                card.addClass('card my-5');
+                card.attr('id', "article-id-" + articleNumber)
+                $(".article-container").append(card);
+                
                 if (result.response.docs[i].headline !== "null") {
                     //get headline.main and inject it into HTML
+                    $("#article-id-" + articleNumber).append(`<div class='card-header num${articleNumber}'><h3>${title}</h3></div>`)
                     console.log(result.response.docs[i].headline.main)
                 }
                 if (result.response.docs[i].byline && author) {
                     console.log(author)
+                    $(".num" + articleNumber).append(`<p>Written by : ${author}</p>`)
                 }
                 if(image){
                     console.log(`https://static01.nyt.com/${image}`)
+                    $("#article-id-" + articleNumber).prepend(`<img src='https://static01.nyt.com/${image}'/>`)
+                }
+                //Links to articles
+                if(link){
+                    console.log(link);
+                    $("#article-id-" + articleNumber).append(`<div class='card-body card-body-num${articleNumber} text-center'><a class='btn btn-outline-danger' href='${link}'>GO TO ARTICLE</a></div>`)
+                }
+                //Article details 
+                if(snippet){
+                    console.log(snippet);
+                    $(".card-body-num" + articleNumber).prepend(`<p>${snippet}</p>`)
                 }
             }
         })
---
+++ readme.md
@@ -0,0 +1,10 @@
+# NYT_SEARCH 
+
+Using AJAX and NYT API to send request using NYT parmas, and utilize received data.
+1. Get user search values from HTML form
+2. add user's values to NYT search params
+3. Receive results and Display results in HTML page.
+
+# Made using : 
+
+HTML / BOOTSTRAP / JavaScript / jQuery(AJAX)
